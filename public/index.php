49df089cf83740307e611e7edd3207fa2095ed54
gangardar <ggdgangardarggd@gmail.com>
2023-09-01 09:21:25 +0630

    Composer installed and pest testing plugin installed

--- public/index.php
+++ public/index.php
@@ -7,17 +7,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
 
 const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . '/vendor/autoload.php';
+
+
+session_start();
 
 require BASE_PATH . "Core/functions.php";
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
 
-    require basePath("{$class}.php");
-});
+
+// spl_autoload_register(function ($class) {
+//     $class = str_replace('\\', '/', $class);
+
+//     require basePath("{$class}.php");
+// });
+
+
 
 require basePath('bootstrap.php');
 
